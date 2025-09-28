82ae3ab227451adf58c0d44769428133dd7ffb4e
kapybara <97310663+Kimmm-c@users.noreply.github.com>
2025-09-28 15:45:48 -0700

    created dummyDb enum

---
+++ backend/src/enums/dummy-db.js
@@ -0,0 +1,229 @@
+const DummyDB = {
+    questions: [
+   {
+      "questionId":1,
+      "content":"Donec dapibus. Duis at velit eu est congue elementum.",
+      "upVotes":1,
+      "downVotes":1,
+      "createdAt":"5/3/2025",
+      "updatedAt":"5/26/2025",
+      "isAnonymous":false,
+      "userId":1,
+      "courseId":1
+   },
+   {
+      "questionId":2,
+      "content":"Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci.",
+      "upVotes":2,
+      "downVotes":2,
+      "createdAt":"12/24/2024",
+      "updatedAt":"10/27/2024",
+      "isAnonymous":true,
+      "userId":2,
+      "courseId":2
+   },
+   {
+      "questionId":3,
+      "content":"Nulla nisl. Nunc nisl. Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.",
+      "upVotes":3,
+      "downVotes":3,
+      "createdAt":"10/21/2024",
+      "updatedAt":"3/24/2025",
+      "isAnonymous":true,
+      "userId":3,
+      "courseId":3
+   },
+   {
+      "questionId":4,
+      "content":"Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+      "upVotes":4,
+      "downVotes":4,
+      "createdAt":"7/19/2025",
+      "updatedAt":"7/18/2025",
+      "isAnonymous":true,
+      "userId":4,
+      "courseId":4
+   },
+   {
+      "questionId":5,
+      "content":"Pellentesque ultrices mattis odio. Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus. Curabitur at ipsum ac tellus semper interdum.",
+      "upVotes":5,
+      "downVotes":5,
+      "createdAt":"3/24/2025",
+      "updatedAt":"12/13/2024",
+      "isAnonymous":false,
+      "userId":5,
+      "courseId":5
+   },
+   {
+      "questionId":6,
+      "content":"Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
+      "upVotes":6,
+      "downVotes":6,
+      "createdAt":"8/19/2025",
+      "updatedAt":"6/24/2025",
+      "isAnonymous":false,
+      "userId":6,
+      "courseId":6
+   },
+   {
+      "questionId":7,
+      "content":"Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia.",
+      "upVotes":7,
+      "downVotes":7,
+      "createdAt":"12/24/2024",
+      "updatedAt":"10/31/2024",
+      "isAnonymous":false,
+      "userId":7,
+      "courseId":7
+   },
+   {
+      "questionId":8,
+      "content":"Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.",
+      "upVotes":8,
+      "downVotes":8,
+      "createdAt":"3/17/2025",
+      "updatedAt":"10/12/2024",
+      "isAnonymous":false,
+      "userId":8,
+      "courseId":8
+   },
+   {
+      "questionId":9,
+      "content":"Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum.",
+      "upVotes":9,
+      "downVotes":9,
+      "createdAt":"5/28/2025",
+      "updatedAt":"10/23/2024",
+      "isAnonymous":false,
+      "userId":9,
+      "courseId":9
+   },
+   {
+      "questionId":10,
+      "content":"Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc.",
+      "upVotes":10,
+      "downVotes":10,
+      "createdAt":"9/27/2025",
+      "updatedAt":"12/5/2024",
+      "isAnonymous":false,
+      "userId":10,
+      "courseId":10
+   }
+],
+
+answers: [
+   {
+      "answerId":1,
+      "questionId":1,
+      "content":"Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.",
+      "upVotes":1,
+      "downVotes":1,
+      "createdAt":"2/5/2025",
+      "updatedAt":"5/10/2025",
+      "isAnonymous":false,
+      "userId":1
+   },
+   {
+      "answerId":2,
+      "questionId":2,
+      "content":"Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.",
+      "upVotes":2,
+      "downVotes":2,
+      "createdAt":"1/19/2025",
+      "updatedAt":"7/6/2025",
+      "isAnonymous":true,
+      "userId":2
+   },
+   {
+      "answerId":3,
+      "questionId":3,
+      "content":"Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh. In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.",
+      "upVotes":3,
+      "downVotes":3,
+      "createdAt":"2/14/2025",
+      "updatedAt":"2/1/2025",
+      "isAnonymous":true,
+      "userId":3
+   },
+   {
+      "answerId":4,
+      "questionId":4,
+      "content":"Nunc nisl. Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum. In hac habitasse platea dictumst.",
+      "upVotes":4,
+      "downVotes":4,
+      "createdAt":"9/23/2025",
+      "updatedAt":"7/17/2025",
+      "isAnonymous":false,
+      "userId":4
+   },
+   {
+      "answerId":5,
+      "questionId":5,
+      "content":"Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat. Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede. Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+      "upVotes":5,
+      "downVotes":5,
+      "createdAt":"6/24/2025",
+      "updatedAt":"12/24/2024",
+      "isAnonymous":false,
+      "userId":5
+   },
+   {
+      "answerId":6,
+      "questionId":6,
+      "content":"Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante.",
+      "upVotes":6,
+      "downVotes":6,
+      "createdAt":"4/25/2025",
+      "updatedAt":"6/5/2025",
+      "isAnonymous":false,
+      "userId":6
+   },
+   {
+      "answerId":7,
+      "questionId":7,
+      "content":"Pellentesque ultrices mattis odio.",
+      "upVotes":7,
+      "downVotes":7,
+      "createdAt":"7/23/2025",
+      "updatedAt":"11/15/2024",
+      "isAnonymous":false,
+      "userId":7
+   },
+   {
+      "answerId":8,
+      "questionId":8,
+      "content":"In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.",
+      "upVotes":8,
+      "downVotes":8,
+      "createdAt":"3/28/2025",
+      "updatedAt":"10/4/2024",
+      "isAnonymous":false,
+      "userId":8
+   },
+   {
+      "answerId":9,
+      "questionId":9,
+      "content":"Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh. In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.",
+      "upVotes":9,
+      "downVotes":9,
+      "createdAt":"9/15/2025",
+      "updatedAt":"10/3/2024",
+      "isAnonymous":false,
+      "userId":9
+   },
+   {
+      "answerId":10,
+      "questionId":10,
+      "content":"Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh. In quis justo.",
+      "upVotes":10,
+      "downVotes":10,
+      "createdAt":"5/23/2025",
+      "updatedAt":"3/14/2025",
+      "isAnonymous":false,
+      "userId":10
+   }
+]    
+}
+
+module.exports = DummyDB;
