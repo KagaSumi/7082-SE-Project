87fff85bf482e936e087b794dc2f5dbfc929ce2f
Quinten Leung <47546786+KagaSumi@users.noreply.github.com>
2025-10-30 14:40:05 -0700

    Tag Support for Update Question (#11)
    * Remove these routes as users should not be able to run them
    
    * Restore routes just added comments
    
    * Support for tags in questions update

--- backend/src/routes/tag-router.js
+++ backend/src/routes/tag-router.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const tagController = require("../controllers/tag-controller");
 
-router.post("/", tagController.createTag);
+router.post("/", tagController.createTag); //Not meant to be called in frontend
 router.get("/", tagController.getAllTags);
 router.get("/:id", tagController.getTagById);
-router.put("/:id", tagController.updateTag);
-router.delete("/:id", tagController.deleteTag);
+router.put("/:id", tagController.updateTag); //Not meant to be called in frontend
+router.delete("/:id", tagController.deleteTag); //Not meant to be called in frontend
 
 module.exports = router;
 
