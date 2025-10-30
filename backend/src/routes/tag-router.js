25c2a4211594bc5e8e980d1e798955b06a7665fb
Quinten Leung <Quinten.Leung@gmail.com>
2025-10-30 10:56:05 -0700

    Remove these routes as users should not be able to run them

--- backend/src/routes/tag-router.js
+++ backend/src/routes/tag-router.js
@@ -2,11 +2,8 @@ const express = require("express");
 const router = express.Router();
 const tagController = require("../controllers/tag-controller");
 
-router.post("/", tagController.createTag);
 router.get("/", tagController.getAllTags);
 router.get("/:id", tagController.getTagById);
-router.put("/:id", tagController.updateTag);
-router.delete("/:id", tagController.deleteTag);
 
 module.exports = router;
 
